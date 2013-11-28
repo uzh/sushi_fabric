0a514c987d50f4625456a49a099810e9eb245e59
Masa <masaomi.hatakeyama@gmail.com>
2013-11-28 16:30:56 +0100

    Verion 0.1.0, for publishing, changed save_data_set method position

--- lib/sushi_fabric/sushiApp.rb
+++ lib/sushi_fabric/sushiApp.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-# Version = '20131115-105530'
+# Version = '20131128-161947'
 
 require 'csv'
 require 'fileutils'
@@ -118,6 +118,40 @@ class ::String
     scan(/\[(.*)\]/).flatten.join =~ /#{tag}/
   end
 end
+def save_data_set(data_set_arr, headers, rows)
+  data_set_hash = Hash[*data_set_arr]
+  if project = Project.find_by_number(data_set_hash['ProjectNumber'].to_i)
+    data_set = DataSet.new
+    data_set.name = data_set_hash['DataSetName']
+    data_set.project = project
+    if parent_id = data_set_hash['ParentID'] and parent_data_set = DataSet.find_by_id(parent_id.to_i)
+      data_set.data_set = parent_data_set
+    end
+    if comment = data_set_hash['Comment'] and !comment.to_s.empty?
+      data_set.comment = comment
+    end
+
+    sample_hash = {}
+    rows.each do |row|
+      headers.each_with_index do |header, i|
+       sample_hash[header]=row[i]
+      end
+      sample = Sample.new
+      sample.key_value = sample_hash.to_s
+      sample.save unless sample.saved?
+      data_set.samples << sample
+    end
+
+    data_set.md5 = data_set.md5hexdigest
+    unless data_set.saved?
+      project.data_sets << data_set
+      parent_data_set.data_sets << data_set if parent_data_set
+      data_set.save
+    end
+    data_set.id
+  end
+end
+
 class SushiApp
   attr_reader :params
   attr_reader :job_ids
@@ -424,7 +458,6 @@ rm -rf #{@scratch_dir} || exit 1
       data_set.id
     end
   end
-
   def run
     test_run
 
